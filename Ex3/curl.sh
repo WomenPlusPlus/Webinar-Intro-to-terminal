98b4d353779e95388a44a8a2fe35158def67a667
Marta D'Angelo <marta.dangelo@gssi.infn.it>
2020-05-06 12:08:32 +0200

    Add curl command file

---
+++ Ex3/curl.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo -e "Let see how to transfer data using the terminal"
+read next
+echo -e "Type on the terminal: man curl"
+read next
+echo -e "This command allows you to tranfer data from or to a server by using a specific protocol "
+read next
+echo -e "Did't quite get it? Let's try with an example"
+read next
+echo -e "Type on the termial: curl <address of the file to download> --output <where to save on your laptop>"
+echo -e "curl https://raw.githubusercontent.com/WomenPlusPlus/Webinar-Intro-to-terminal/master/Ex1/0_Shell-Prompt-Shell-Scripts.sh --output ./Desktop/readme.sh"
